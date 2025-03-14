ea04fbb40cf2c547b8bd3d1808bfc94b870b88fe
lisa-arvis <106652051+lisa-arvis@users.noreply.github.com>
2025-03-14 14:22:16 +0300

    Update TweenTMPTextEditors.cs
    Fixed editor clickable area issue

--- Assets/TweenPlayables/Editor/Editors/UI/TweenTMPTextEditors.cs
+++ Assets/TweenPlayables/Editor/Editors/UI/TweenTMPTextEditors.cs
@@ -5,30 +5,58 @@ using UnityEditor.Timeline;
 
 namespace TweenPlayables.Editor
 {
-    [CustomTimelineEditor(typeof(TweenTMPTextTrack))]
-    public sealed class TweenTMPTextTrackEditor : TweenAnimationTrackEditor
+    [CustomTimelineEditor(typeof(TweenTextMeshProUGUITrack))]
+    public class TweenTextMeshProUGUITrackEditor : TweenAnimationTrackEditor
     {
-        public override Color TrackColor => Styles.UGUIColor;
-        public override Texture2D TrackIcon => Styles.TextMeshProUGUIIcon;
-        public override string DefaultTrackName => "Tween TMP_Text Track";
+        public override Color trackColor => Styling.uguiColor;
+        public override Texture2D trackIcon => Styling.textMeshProUGUIIcon;
+        public override string defaultTrackName => "Tween TextMeshProUGUI Track";
     }
 
-    [CustomTimelineEditor(typeof(TweenTMPTextClip))]
-    public sealed class TweenTMPTextClipEditor : TweenAnimationClipEditor
+    [CustomTimelineEditor(typeof(TweenTextMeshProUGUIClip))]
+    public class TweenTextMeshProUGUIClipEditor : TweenAnimationClipEditor
     {
-        public override string DefaultClipName => "Tween TMP_Text";
-        public override Color ClipColor => Styles.UGUIColor;
-        public override Texture2D ClipIcon => Styles.TextMeshProUGUIIcon;
+        public override string defaultClipName => "Tween TextMeshProUGUI";
+        public override Color clipColor => Styling.uguiColor;
+        public override Texture2D clipIcon => Styling.textMeshProUGUIIcon;
     }
 
-    [CustomPropertyDrawer(typeof(TweenTMPTextBehaviour))]
-    public sealed class TweenTMPTextBehaviourDrawer : TweenAnimationBehaviourDrawer
+    [CustomPropertyDrawer(typeof(TweenTextMeshProUGUIBehaviour))]
+    public class TweenTextMeshProUGUIBehaviourDrawer : PropertyDrawer
     {
-        static readonly string[] parameters = new string[]
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
-            "color", "fontSize", "characterSpacing", "lineSpacing", "wordSpacing", "paragraphSpacing", "colorGradient", "text"
-        };
+            position.y += 7f;
+            GUIHelper.Field(ref position, property.FindPropertyRelative("color"));
+            position.y += 2f;
+            GUIHelper.Field(ref position, property.FindPropertyRelative("fontSize"));
+            position.y += 2f;
+            GUIHelper.Field(ref position, property.FindPropertyRelative("characterSpacing"));
+            position.y += 2f;
+            GUIHelper.Field(ref position, property.FindPropertyRelative("lineSpacing"));
+            position.y += 2f;
+            GUIHelper.Field(ref position, property.FindPropertyRelative("wordSpacing"));
+            position.y += 2f;
+            GUIHelper.Field(ref position, property.FindPropertyRelative("paragraphSpacing"));
+            position.y += 2f;
+            GUIHelper.Field(ref position, property.FindPropertyRelative("colorGradient"));
+            position.y += 2f;
+            GUIHelper.Field(ref position, property.FindPropertyRelative("text"));
+        }
 
-        protected override IEnumerable<string> GetPropertyNames() => parameters;
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            float height = 27f;
+            height += EditorGUI.GetPropertyHeight(property.FindPropertyRelative("color"));
+            height += EditorGUI.GetPropertyHeight(property.FindPropertyRelative("fontSize"));
+            height += EditorGUI.GetPropertyHeight(property.FindPropertyRelative("characterSpacing"));
+            height += EditorGUI.GetPropertyHeight(property.FindPropertyRelative("lineSpacing"));
+            height += EditorGUI.GetPropertyHeight(property.FindPropertyRelative("wordSpacing"));
+            height += EditorGUI.GetPropertyHeight(property.FindPropertyRelative("paragraphSpacing"));
+            height += EditorGUI.GetPropertyHeight(property.FindPropertyRelative("colorGradient"));
+            height += EditorGUI.GetPropertyHeight(property.FindPropertyRelative("text"));
+
+            return height;
+        }
     }
 }
